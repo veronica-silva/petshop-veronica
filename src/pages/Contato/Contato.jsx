2cd03ccb53c2c032fc042a5e6256ede44c9afb29
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-20 09:26:26 -0300

    criação dos demais components

---
+++ src/pages/Contato/Contato.jsx
@@ -0,0 +1,10 @@
+import estilos from "./Contato.module.css";
+const Contato = () => {
+  return (
+    <section>
+      <h2 className={estilos.titulo_secao}>Fale conosco</h2>
+    </section>
+  );
+};
+
+export default Contato;
---
+++ src/pages/Sobre/Sobre.jsx
@@ -0,0 +1,10 @@
+import estilos from "./Sobre.module.css";
+const Sobre = () => {
+  return (
+    <section>
+      <h2 className={estilos.titulo_secao}>Pet Notícias</h2>
+    </section>
+  );
+};
+
+export default Sobre;
