aa02c6d4371597ece045de526dfcab6a21bc4281
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-24 10:10:26 -0300

    arrumando cagadinhas xD

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -1,10 +1,11 @@
 import estilos from "./ListaPosts.module.css";
-
+import serverApi from "../../api/servidor-api";
 const ListaPosts = () => {
-  const posts = [];
+  console.log(serverApi);
+  const postsTemp = [];
   return (
     <div className={estilos.lista_posts}>
-      {posts.map(({ id, titulo, subtitulo, descricao, categoria }) => (
+      {postsTemp.map(({ id, titulo, subtitulo, descricao, categoria }) => (
         <article className={estilos.post} key={id}>
           <h3>{titulo}</h3>
           <h4>{subtitulo}</h4>
