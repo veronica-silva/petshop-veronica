829f02329770e8409bcafcdd34b41e3958821977
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-24 10:41:16 -0300

    async/await

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -2,7 +2,21 @@ import estilos from "./ListaPosts.module.css";
 import serverApi from "../../api/servidor-api";
 const ListaPosts = () => {
   console.log(serverApi);
+
   const postsTemp = [];
+
+  const getPosts = async () => {
+    try {
+      const resposta = await fetch(`${serverApi}/posts`);
+      const dados = await resposta.json();
+      console.log(dados);
+    } catch (error) {
+      console.log("Deu ruim aí hein chapa " + error.message);
+    }
+  };
+
+  getPosts();
+
   return (
     <div className={estilos.lista_posts}>
       {postsTemp.map(({ id, titulo, subtitulo, descricao, categoria }) => (
