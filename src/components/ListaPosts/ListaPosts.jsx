9c68d5bc4d4fdc1de96256fd96b77754485ec0c5
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-24 11:09:54 -0300

    dependencias useEffect

--- src/components/ListaPosts/ListaPosts.jsx
+++ src/components/ListaPosts/ListaPosts.jsx
@@ -1,25 +1,28 @@
+import { useState, useEffect } from "react";
 import estilos from "./ListaPosts.module.css";
 import serverApi from "../../api/servidor-api";
 const ListaPosts = () => {
+  const [posts, setPosts] = useState([]);
   console.log(serverApi);
 
   const postsTemp = [];
 
-  const getPosts = async () => {
-    try {
-      const resposta = await fetch(`${serverApi}/posts`);
-      const dados = await resposta.json();
-      console.log(dados);
-    } catch (error) {
-      console.log("Deu ruim aí hein chapa " + error.message);
+  useEffect(() => {
+    async function getPosts() {
+      try {
+        const resposta = await fetch(`${serverApi}/posts`);
+        const dados = await resposta.json();
+        setPosts(dados);
+      } catch (error) {
+        console.log("Deu ruim aí hein chapa " + error.message);
+      }
     }
-  };
-
-  getPosts();
+    getPosts();
+  }, []);
 
   return (
     <div className={estilos.lista_posts}>
-      {postsTemp.map(({ id, titulo, subtitulo, descricao, categoria }) => (
+      {posts.map(({ id, titulo, subtitulo, descricao, categoria }) => (
         <article className={estilos.post} key={id}>
           <h3>{titulo}</h3>
           <h4>{subtitulo}</h4>
