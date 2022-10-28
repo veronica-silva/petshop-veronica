08c092c1416eff1fc6a9d6237e295fc56d2090c4
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-10-28 09:39:11 -0300

    estilizando formulário com a biblioteca MUI

--- src/pages/Contato/Contato.jsx
+++ src/pages/Contato/Contato.jsx
@@ -1,16 +1,47 @@
 import estilos from "./Contato.module.css";
 import Caixa from "../../components/Caixa/Caixa";
+import { TextField, Button } from "@mui/material";
 const Contato = () => {
   return (
     <section>
       <h2 className={estilos.titulo_secao}>Fale conosco</h2>
       <Caixa>
-        <p>em breve</p>
-        <p>teremos</p>
-        <p>um formulário</p>
-        <p>através</p>
-        <p>de uma biblioteca</p>
-        <p>de componentes</p>
+        <form method="post" className={estilos.formulario}>
+          <div>
+            <TextField
+              helperText="Informe o seu nome"
+              type="text"
+              fullWidth
+              id="outlined-basic"
+              label="Nome"
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              helperText="Informe o seu e-mail"
+              type="email"
+              fullWidth
+              id="outlined-basic"
+              label="Email"
+              variant="outlined"
+            />
+          </div>
+          <div>
+            <TextField
+              helperText="Escreva a sua mensagem"
+              type="text"
+              fullWidth
+              id="outlined-multiline-static"
+              label="Mensagem"
+              multiline
+              rows={6}
+            />
+          </div>
+          <div>
+            <Button variant="outlined">Enviar Mensagem</Button>
+          </div>
+        </form>
       </Caixa>
     </section>
   );
