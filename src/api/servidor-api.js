08aab66159b433f51beac2593990c9497dbb922d
Veronica Silva <78879969+veronica-silva@users.noreply.github.com>
2022-11-01 09:27:56 -0300

    acesso via rede
    configurações de ip para realizar testes via rede no computador e celular

--- src/api/servidor-api.js
+++ src/api/servidor-api.js
@@ -1,4 +1,4 @@
 const porta = "2112";
-const serverApi = `http://localhost:${porta}`;
-
+/* const serverApi = `http://localhost:${porta}`; */
+const serverApi = `http://10.20.45.22:${porta}`;
 export default serverApi;
